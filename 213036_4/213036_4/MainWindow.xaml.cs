a68cb87f6907d1dd5243f99624006d069682f00f
Zain213036 <hafizzain897@gmail.com>
2023-12-08 15:10:31 +0500

    213036_Assignment4

---
+++ 213036_4/213036_4/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace WpfOneWayBindingExample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Create an instance of the Person class
+            Person person = new Person
+            {
+                Name = "Zain",
+                Age = 21,
+                Occupation = "Student"
+            };
+
+            // Set the DataContext of the window to the instance of the Person class
+            DataContext = person;
+        }
+    }
+}
---
+++ 213036_4/213036_4/Person.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+
+namespace WpfOneWayBindingExample
+{
+    public class Person : INotifyPropertyChanged
+    {
+        private string name;
+        private int age;
+        private string occupation;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (age != value)
+                {
+                    age = value;
+                    OnPropertyChanged("Age");
+                }
+            }
+        }
+
+        public string Occupation
+        {
+            get { return occupation; }
+            set
+            {
+                if (occupation != value)
+                {
+                    occupation = value;
+                    OnPropertyChanged("Occupation");
+                }
+            }
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
